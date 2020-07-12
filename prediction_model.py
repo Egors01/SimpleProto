20a26e385a282dcefa924723b5397c98b921198d
egors01 <egorsemenchenko@gmail.com>
2020-07-12 10:24:56 +0200

    polishing and plotting

--- prediction_model.py
+++ prediction_model.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from constants import *
-
+from tree_parser import DecisionTreeParser
 print(os.getcwd())
 
 
@@ -16,32 +16,30 @@ clf = DecisionTreeClassifier(random_state = 0,max_features=5)
 df = pd.read_csv('data_table.tsv',sep='\t')
 X_train = df[df.columns.values[3:-4]]
 Y_train = df['target']
-
 clf.fit(X_train, Y_train)
-
 string = export_text(clf)
+questions = [questions_dict[name] for name in feature_names]
 
-
-fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (35,30), dpi=300)
+# Plot TREE
+fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (50,50), dpi=200)
 plot_tree(clf,
-               feature_names = feature_names,
-               class_names=df['name'],
-               filled = True,
-                fontsize=14,impurity=False)
+               feature_names = questions,
+               class_names=df['name'],filled=True,proportion=False,
+                fontsize=20,node_ids = True,rounded=True)
 plt.savefig('decision_tree.png')
 plt.show()
 
-#
-#
-# from tree_parser import DecisionTreeParser
-# decision_guide = DecisionTreeParser(string,feature_names)
-# result_clf = decision_guide.get_first_question()
-# seq = []
-# i=0
-#
-# print('info: is leaf', result_clf.is_leaf, 'QQ: ', result_clf.current_question, 'QN: ', result_clf.next_question)
-# while not result_clf.is_leaf:
-#     result_clf = decision_guide.update_classification(seq)
-#
-# print('Result ', result_clf.is_leaf, result_clf.target_name, 'in ', i)
-#
+# Follow the path on the TREE
+
+
+decision_guide = DecisionTreeParser(string,feature_names)
+result_clf = decision_guide.get_first_question()
+seq = []
+i=0
+
+print('info: is leaf', result_clf.is_leaf, 'QQ: ', result_clf.current_question, 'QN: ', result_clf.next_question)
+while not result_clf.is_leaf:
+    result_clf = decision_guide.update_classification(seq)
+
+print('Result ', result_clf.is_leaf, result_clf.target_name, 'in ', i)
+
